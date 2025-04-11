8d8d5c9fe623756d8fb18a9813ea016b89d5e10e
Tyrese987 <22118061@cambria.ac.uk>
2025-04-11 12:04:50 +0100

    Update Gym system 1.9.py

--- Gym system 1.9.py
+++ Gym system 1.9.py
@@ -5,7 +5,11 @@ import re
 import random
 import os
 import datetime
-from datetime
+
+
+
+# Classes will be defined here for the system Currently Staff and Customer 
+from datetime import datetime
 
 class Customer:
     def __init__(self, userid, fname, sname, phone, email, dob, health_issues):
